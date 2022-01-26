81b9fa8ff80cfed9fbea3da7d827402ba2e2bc3f
Weaact <lucas.druckes@numericable.fr>
2022-01-26 11:51:26 +0100

    game done

--- Scene/Game/Game.gd
+++ Scene/Game/Game.gd
@@ -6,6 +6,7 @@ func get_class() -> String: return "Game"
 onready var achievement_label : Label = get_node("Achievements/CanvasLayer/AchievementPanel/AchievementName")
 onready var achievement_animationplayer : AnimationPlayer = get_node("Achievements/AnimationPlayer")
 
+onready var universe_area_node : Area2D = get_node("UniverseLimit")
 #### ACCESSORS ####
 
 #### BUILT-IN ####
@@ -14,6 +15,8 @@ func _ready() -> void:
 	EVENTS.ach_label = achievement_label
 	EVENTS.ach_player = achievement_animationplayer
 	EVENTS.ach_anim_name = "display_achievement"
+	
+	var __ = universe_area_node.connect("body_exited", self, "_on_body_exited_universe_area")
 
 #### VIRTUALS ####
 
@@ -25,3 +28,6 @@ func unlock_game_achievement(ach_name : String = "") -> void:
 #### INPUTS ####
 
 #### SIGNAL RESPONSES ####
+func _on_body_exited_universe_area(body: PhysicsBody2D) -> void:
+	if body is Player:
+		EVENTS.unlock_outside_universe()
---
+++ Scene/Networks/Nohead/NoHeadNetwork.gd
@@ -0,0 +1,50 @@
+extends NetworkBase
+class_name NoHeadNetwork
+func is_class(value: String): return value == "NoHeadNetwork" or .is_class(value)
+func get_class() -> String: return "NoHeadNetwork"
+
+onready var itcharea : Area2D = get_node("ItchIO/itcharea")
+onready var twitterarea : Area2D = get_node("Twitter/twitterarea")
+
+onready var itchioplayer_node : AnimationPlayer = get_node("ItchIOPlayer")
+onready var twitterplayer_node : AnimationPlayer = get_node("TwitterPlayer")
+
+func _ready() -> void:
+	var __
+	__ = itcharea.connect("body_entered", self, "_on_body_entered_itch_area")
+	__ = itcharea.connect("body_exited", self, "_on_body_exited_itch_area")
+	
+	__ = twitterarea.connect("body_entered", self, "_on_body_entered_twitter_area")
+	__ = twitterarea.connect("body_exited", self, "_on_body_exited_twitter_area")
+
+#### VIRTUALS ####
+
+#### LOGIC ####
+
+#### INPUTS ####
+
+#### SIGNAL RESPONSES ####
+
+func _on_body_entered_itch_area(body: PhysicsBody2D) -> void:
+	if body is Player:
+		itchioplayer_node.set_speed_scale(1.0)
+		itchioplayer_node.play("itchio_appear")
+		should_open_itch = true
+
+func _on_body_exited_itch_area(body: PhysicsBody2D) -> void:
+	if body is Player:
+		itchioplayer_node.set_speed_scale(3.0)
+		itchioplayer_node.play_backwards("itchio_appear")
+		should_open_itch = false
+
+func _on_body_entered_twitter_area(body: PhysicsBody2D) -> void:
+	if body is Player:
+		twitterplayer_node.set_speed_scale(1.0)
+		twitterplayer_node.play("twitter_appear")
+		should_open_twitter = true
+
+func _on_body_exited_twitter_area(body: PhysicsBody2D) -> void:
+	if body is Player:
+		twitterplayer_node.set_speed_scale(3.0)
+		twitterplayer_node.play_backwards("twitter_appear")
+		should_open_twitter= false
